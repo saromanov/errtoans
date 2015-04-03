a793d739aa593895fc1cf389ee3e4017766a6dc6
Sergey Romanov <xxsmotur@gmail.com>
2015-04-03 23:55:23 +0500

    Append store results to file

--- errtoans.py
+++ errtoans.py
@@ -4,6 +4,7 @@ import py_compile
 import subprocess
 import sys
 import argparse
+import os.path
 
 #https://github.com/lucjon/Py-StackExchange/tree/master/demo
 #http://www.crummy.com/software/BeautifulSoup/bs4/doc/
@@ -11,16 +12,37 @@ import argparse
 def withSort(so):
 	qs = so.search(sort=stackexchange.Sort.Votes, order=stackexchange.DESC)
 
-def parse_answer(data):
+def parse_answer(data, outfile=None):
 	soup = BeautifulSoup(data)
 	if soup.code != None:
-		print(soup.get_text())
+		result = soup.get_text()
+		if outfile != None:
+			write_file(outfile, result)
+		print(result)
 
 
-def get_answers(data):
-	for answer in data: parse_answer(answer.body)
+def get_answers(data, outfile=None):
+	''' Show answer '''
+	for answer in data: parse_answer(answer.body, outfile=outfile)
 
-def get_from_stackoverflow(title,limit=10, byscore=True):
+def prepare_file(path):
+	if os.path.isfile(path):
+		open(path, 'w').close()
+
+def write_file(path, data):
+	""" Store result to file """
+	with open(path, 'a') as f:
+		f.write(data)
+
+def show_info(msg, outfile=None):
+	""" Print message, if outpath not None, write this message here( and print it)
+		Note: Probably better store it to class 
+	"""
+	if outfile != None:
+		write_file(outfile, msg)
+	print(msg)
+
+def get_from_stackoverflow(title,limit=10, byscore=True, outfile=None):
 	'''
 		limit can be less than 10
 		byscore - sorted questions by votes
@@ -33,11 +55,11 @@ def get_from_stackoverflow(title,limit=10, byscore=True):
 	qs = so.search(intitle=title, sort=sortscore)
 	for q in qs:
 		question = so.question(q.id)
-		print(question.url)
-		print(q.title)
-		print("\n")
-		print("Answer: ")
-		get_answers(question.answers)
+		show_info(question.url, outfile=outfile)
+		show_info(q.title, outfile=outfile)
+		show_info("\n", outfile=outfile)
+		show_info("Answer: ", outfile=outfile)
+		get_answers(question.answers, outfile=outfile)
 		#get_code_from_q(question.body)
 
 def main(targetfile, *args, **kwargs):
@@ -47,9 +69,12 @@ def main(targetfile, *args, **kwargs):
 	output = process.communicate()
 	result = str(output[1]).split("\\n")
 	if len(result) > 1:
+		path = kwargs.get('outfile')
+		if path != None:
+			prepare_file(path)
 		value = result[-2]
 		print("Your error: ", value)
-		get_from_stackoverflow(value, limit=kwargs.get('num_answers'))
+		get_from_stackoverflow(value, limit=kwargs.get('num_answers',10), outfile=path)
 		return
 	print("file {0} not contain errors".format(targetfile))
 
@@ -62,6 +87,7 @@ if __name__ == '__main__':
 	parser.add_argument('path', default=sys.argv[1])
 	parser.add_argument('--answers', help='show full answers', action='store_true')
 	parser.add_argument('--num_answers', help='Show first n answers', type=int, default=10)
+	parser.add_argument('--outfile', help='Store results to file')
 	args = parser.parse_args()
-	main(sys.argv[1], answers=args.answers, num_answers=args.num_answers)
+	main(sys.argv[1], answers=args.answers, num_answers=args.num_answers, outfile=args.outfile)
 
