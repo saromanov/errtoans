74903dfac30af64f58a662d3a2e854053a96615b
Sergey Romanov <xxsmotur@gmail.com>
2015-04-06 23:28:48 +0500

    append question body and shown full trace from error message

--- errtoans.py
+++ errtoans.py
@@ -25,6 +25,7 @@ def parse_answer(data, outfile=None):
 
 def get_answers(data, outfile=None):
 	''' Show answer '''
+	print("Answer: \n")
 	for answer in data: parse_answer(answer.body, outfile=outfile)
 
 def prepare_file(path):
@@ -35,14 +36,6 @@ def write_file(path, data):
 	""" Store result to log file """
 	with open(path, 'a') as f:
 		f.write(data)
-
-def show_info(msg, color, outfile=None):
-	""" Print message, if outpath not None, write this message here( and print it)
-		Note: Probably better store it to class 
-	"""
-	if outfile != None:
-		write_file(outfile, msg)
-	print(colored(msg, color))
 
 def get_from_stackoverflow(title,limit=10, byscore=True, outfile=None, api=None):
 	'''
@@ -55,27 +48,44 @@ def get_from_stackoverflow(title,limit=10, byscore=True, outfile=None, api=None)
 	so = stackexchange.Site(stackexchange.StackOverflow, app_key=api, impose_throttling=True)
 	so.be_inclusive()
 	qs = so.search(intitle=title, sort=sortscore)
+
+	def show_info(msg, color):
+		""" Print message, if outpath not None, write this message here( and print it)
+		"""
+		if outfile != None:
+			write_file(outfile, msg)
+		print(colored(msg, color))
+
 	for q in qs:
 		question = so.question(q.id)
-		show_info(question.url, 'green', outfile=outfile)
-		show_info(q.title, 'blue', outfile=outfile)
-		show_info("\n", 'white', outfile=outfile)
-		show_info("Answer: ", 'white', outfile=outfile)
-		get_answers(question.answers, outfile=outfile)
-		#get_code_from_q(question.body)
+		show_info(question.url, 'green')
+		show_info(q.title, 'red')
+		show_info("\n", 'white')
+		show_info('Question: ', 'white')
+		show_info(question.body, 'white')
+		show_info("\n", 'white')
+		get_answers(question.answers)
+
+def prepare_error_msg(msg):
+	print(msg)
 
 def main(targetfile, *args, **kwargs):
 	command = 'python {0}'.format(targetfile)
 	process = subprocess.Popen(command.split(), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
 	process.wait()
 	output = process.communicate()
-	result = str(output[1]).split("\\n")
+	full_trace = str(output[1])
+	result = str(full_trace).split("\\n")
 	if len(result) > 1:
 		path = kwargs.get('outfile')
 		if path != None:
 			prepare_file(path)
+		prepare_error_msg(full_trace)
 		value = result[-2]
-		print("Your error: ", value)
+		if kwargs.get('trace'):
+			prepare_error_msg(full_trace)
+		else:
+			print("Your error: ", value)
 		get_from_stackoverflow(value, limit=kwargs.get('num_answers',10), outfile=path)
 		return
 	print("file {0} not contain errors".format(targetfile))
@@ -91,5 +101,7 @@ if __name__ == '__main__':
 	parser.add_argument('--num_answers', help='Show first n answers', type=int, default=10)
 	parser.add_argument('--outfile', help='Store results to file')
 	parser.add_argument('--api', help='Set key to api from StackExchange')
+	parser.add_argument('--show-full-trace', help='Show full stack trace with errors', action='store_false')
 	args = parser.parse_args()
-	main(sys.argv[1], answers=args.answers, num_answers=args.num_answers, outfile=args.outfile, api=args.api)
+	main(sys.argv[1], answers=args.answers, num_answers=args.num_answers, outfile=args.outfile, api=args.api, \
+		trace=args.show_full_trace)
